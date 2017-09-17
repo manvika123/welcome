f99cb6d61e0a2541e604e7df676b61265e81d102
manvika123 <manvikajaiswal1995@gmail.com>
2017-09-17 13:20:47 +0530

    first commit

---
+++ src/Wel/Welcome.java
@@ -0,0 +1,15 @@
+package Wel;
+
+public class Welcome {
+
+
+    public static void main(String[] args) {
+
+
+    System.out.println("Welcome Java");
+    System.out.println("Welcome Java");
+    System.out.println("Welcome Java");
+    System.out.println("Welcome Java");
+    System.out.println("Welcome Java");
+}
+    }
